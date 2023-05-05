7363a2d53120af6259d4b3166823db89c4388de5
falnadashkc <113893312+falnadashkc@users.noreply.github.com>
2023-05-05 15:06:55 +0700

    layout

---
+++ app/src/main/java/kc/si6b/kampuskita/Activity/TambahActivity.java
@@ -0,0 +1,16 @@
+package kc.si6b.kampuskita.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kc.si6b.kampuskita.R;
+
+public class TambahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah);
+    }
+}
---
+++ app/src/main/java/kc/si6b/kampuskita/Activity/UbahActivity.java
@@ -0,0 +1,16 @@
+package kc.si6b.kampuskita.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kc.si6b.kampuskita.R;
+
+public class UbahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah);
+    }
+}
